798e533797e74fbb034ce366d87f2016be29e39f
yogi1095 <yogi1095@gmail.com>
2023-01-11 12:00:57 -0500

    day4 chnages

---
+++ Training/src/com/day4/ArraysExample.java
@@ -0,0 +1,25 @@
+package com.day4;
+
+import java.util.List;
+//default import statement
+//import java.lang.String;
+public class ArraysExample {
+	int a=10;
+	public static void main(String[] args) {
+		ArraysExample arraysExample=new ArraysExample();
+		System.out.println(arraysExample);
+		//int numbers[]= {1,4,6,7,8,8,2345};
+		int numbers2[]=new int[5];//allocataing
+		numbers2[1]=90;
+		numbers2[0]=89;
+		
+		System.out.println(numbers2);
+		List l;
+		
+		
+	}
+
+}
+//int -0
+//float-0.0
+//string -null
---
+++ Training/src/com/day4/LoopsExample.java
@@ -0,0 +1,17 @@
+package com.day4;
+
+public class LoopsExample {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(3 * i);
+		}
+
+		for (int i = 10; i >= 1; i--) {
+			// System.out.println(i);
+		}
+	}
+}
+
+//2*1=2
+//2*2=4
+//2*3=6
